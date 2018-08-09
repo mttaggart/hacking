cdf2fece37364fc06984d8bd399d74e705320ae5
mttaggart <michael.t.taggart@gmail.com>
2018-08-08 21:05:13 -0700

    Add dump function

--- ch2/hacking.h
+++ ch2/hacking.h
@@ -16,3 +16,27 @@ void *ec_malloc(unsigned int size) {
     }
     return ptr;
 }
+
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+    unsigned char byte;
+    unsigned int i, j;
+    for(i=0; i < length; i++) {
+        byte = data_buffer[i];
+        printf("%02x ", data_buffer[i]);
+        if((i % 16 == 15) || (i == length - 1)) {
+            for(j=0; j < 15-(i % 16); j++) {
+                printf("  ");
+            }
+            printf("| ");
+            for(j=(i-(i % 16)); j <= i; j++) {
+                byte = data_buffer[j];
+                if((byte > 31) && (byte < 127)) {
+                    printf("%c", byte);
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+    }
+}
